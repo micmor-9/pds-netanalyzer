a334e1e8efb5806fa843d571ec3fab6743497186
micmor-9 <michelemorgigno@gmail.com>
2022-11-29 18:16:00 +0100

    Finished parser.rs

---
+++ src/lib/error.rs
@@ -0,0 +1,35 @@
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug)]
+pub enum ParserError {
+    EthernetPacketUnrecognized,
+    EthernetPacketError,
+    ArpPacketError,
+    IPv4PacketError,
+    IPv6PacketError,
+    TransportProtocolError,
+    TCPSegmentError,
+    UDPDatagramError,
+    ICMPPacketError,
+    GenericError
+}
+
+impl std::error::Error for ParserError {}
+
+impl Display for ParserError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<T, E> {
+        match self {
+            ParserError::EthernetPacketUnrecognized => write!(f, "[Parser] Packet not recognized!"),
+            ParserError::EthernetPacketError => write!(f, "[Parser] Ethernet Packet error!"),
+            ParserError::ArpPacketError => write!(f, "[Parser] ARP Packet error!"),
+            ParserError::IPv4PacketError => write!(f, "[Parser] IPv4 Packet error!"),
+            ParserError::IPv6PacketError => write!(f, "[Parser] IPv6 Packet error!"),
+            ParserError::IPv6PacketError => write!(f, "[Parser] Transport Protocol error!"),
+            ParserError::TCPSegmentError => write!(f, "[Parser] TCP Segment error!"),
+            ParserError::UDPDatagramError => write!(f, "[Parser] UDP Datagram error!"),
+            ParserError::UDPDatagramError => write!(f, "[Parser] ICMP Packet error!"),
+            ParserError::GenericError => write!(f, "[Parser] Generic error!"),
+            _ => write!(f, "[Parser] Generic error!")
+        }
+    }
+}
--- src/lib/parser.rs
+++ src/lib/parser.rs
@@ -1,29 +1,18 @@
-use std::error::{Error};
-use std::fmt::{Debug, Display, Formatter, Result};
+use std::fmt::{Debug, Display, Formatter};
 use std::net::IpAddr;
 use pcap::Device;
+use pktparse::arp::parse_arp_pkt;
 use pktparse::ethernet::{EtherType, parse_ethernet_frame};
+use chrono;
+use pktparse::ip::IPProtocol;
+use pktparse::ipv4::parse_ipv4_header;
+use pktparse::ipv6::parse_ipv6_header;
+use pktparse::tcp::parse_tcp_header;
+use pktparse::udp::parse_udp_header;
+use pktparse::icmp::{IcmpCode, parse_icmp_header};
 
 use serde::Serialize;
-
-#[derive(Debug)]
-pub enum ParserError {
-    EthernetPacketUnrecognized,
-    EthernetPacketError,
-    GenericError
-}
-
-impl Error for ParserError {}
-
-impl Display for ParserError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        match self {
-            ParserError::EthernetPacketUnrecognized => write!(f, "[Parser] Packet not recognized!"),
-            ParserError::EthernetPacketError => write!(f, "[Parser] Parsing error!"),
-            ParserError::GenericError => write!(f, "[Parser] Generic error!")
-        }
-    }
-}
+use crate::error::ParserError;
 
 #[derive(Debug, Serialize)]
 pub struct Packet {
@@ -65,7 +54,7 @@ impl Packet {
 }
 
 impl Display for Packet {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         todo!()
     }
 }
@@ -88,5 +77,221 @@ pub fn ethernet_frame(interface: &Device, ethernet: &[u8]) -> Result<Packet, Par
             }
         },
         Err(_) => Err(ParserError::EthernetPacketError)
+    }
+}
+
+fn arp_packet(interface_name: &str, packet: &[u8]) -> Result<Packet, ParserError> {
+    let parsed_arp_packet = parse_arp_pkt(packet);
+
+    match parsed_arp_packet {
+        Ok(result) => {
+            let header = result.1;
+
+            Ok(Packet::new(
+                interface_name.to_string(),
+                IpAddr::from(header.src_addr),
+                IpAddr::from(header.dest_addr),
+                None,
+                None,
+                packet.len() as u16,
+                "ARP".to_string(),
+                "".to_string(),
+                chrono::offset::Local::now().to_string()
+            ))
+        },
+        Err(_) => Err(ParserError::ArpPacketError)
+    }
+}
+
+fn ipv6_packet(interface_name: &str, packet: &[u8]) -> Result<Packet, ParserError> {
+    let parsed_ipv6_packet = parse_ipv6_header(packet);
+
+    match parsed_ipv6_packet {
+        Ok(result) => {
+            let payload = result.0;
+            let header = result.1;
+
+            transport_protocol_parser(
+                interface_name,
+                header.next_header,
+                payload,
+                IpAddr::V6(header.source_addr),
+                IpAddr::V6(header.dest_addr)
+            )
+        },
+        Err(_) => Err(ParserError::IPv6PacketError)
+    }
+}
+
+fn ipv4_packet(interface_name: &str, packet: &[u8]) -> Result<Packet, ParserError> {
+    let parsed_ipv4_packet = parse_ipv4_header(packet);
+
+    match parsed_ipv4_packet {
+        Ok(result) => {
+            let payload = result.0;
+            let header = result.1;
+
+            transport_protocol_parser(
+                interface_name,
+                header.protocol,
+                payload,
+                IpAddr::V4(header.source_addr),
+                IpAddr::V4(header.dest_addr)
+            )
+        },
+        Err(_) => Err(ParserError::IPv4PacketError)
+    }
+}
+
+fn tcp_packet(interface_name: &str, source_addr: IpAddr, dest_addr: IpAddr, packet: &[u8]) -> Result<Packet, ParserError> {
+    let parsed_tcp_header = parse_tcp_header(packet);
+
+    match parsed_tcp_header {
+        Ok(result) => {
+            let header = result.1;
+            let application_protocol = application_protocol_parser(&header.dest_port);
+
+            Ok(Packet::new(
+                interface_name.to_string(),
+                source_addr,
+                dest_addr,
+                Some(header.source_port),
+                Some(header.dest_port),
+                packet.len() as u16,
+                "TCP".to_string(),
+                application_protocol,
+                chrono::offset::Local::now().to_string()
+            ))
+        },
+        Err(_) => Err(ParserError::TCPSegmentError)
+    }
+}
+
+fn udp_packet(interface_name: &str, source_addr: IpAddr, dest_addr: IpAddr, packet: &[u8]) -> Result<Packet, ParserError> {
+    let parsed_udp_header = parse_udp_header(packet);
+
+    match parsed_udp_header {
+        Ok(result) => {
+            let header = result.1;
+            let application_protocol = application_protocol_parser(&header.dest_port);
+
+            Ok(Packet::new(
+                interface_name.to_string(),
+                source_addr,
+                dest_addr,
+                Some(header.source_port),
+                Some(header.dest_port),
+                packet.len() as u16,
+                "TCP".to_string(),
+                application_protocol,
+                chrono::offset::Local::now().to_string()
+            ))
+        }
+        Err(_) => Err(ParserError::UDPDatagramError)
+    }
+}
+
+fn icmp_packet(interface_name: &str, source_addr: IpAddr, dest_addr: IpAddr, packet: &[u8]) -> Result<Packet, ParserError> {
+    let parsed_icmp_header = parse_icmp_header(packet);
+
+    match parsed_icmp_header {
+        Ok(result) => {
+            let header = result.1;
+
+            Ok(Packet::new(
+                interface_name.to_string(),
+                source_addr,
+                dest_addr,
+                None,
+                None,
+                packet.len() as u16,
+                ["ICMP", icmp_code_parser(header.code)].join(" - "),
+                "".to_string(),
+                chrono::offset::Local::now().to_string()
+            ))
+        }
+        Err(_) => Err(ParserError::ICMPPacketError)
+    }
+}
+
+fn generic_t_packet(interface_name: &str, source_addr: IpAddr, dest_addr: IpAddr, packet: &[u8]) -> Result<Packet, ParserError> {
+    Ok(Packet::new(
+        interface_name.to_string(),
+        source_addr,
+        dest_addr,
+        None,
+        None,
+        packet.len() as u16,
+        "unknown".to_string(),
+        "unknown".to_string(),
+        chrono::offset::Local::now().to_string()
+    ))
+}
+
+// Function to parse the packet with the correct transport layer
+fn transport_protocol_parser(interface_name: &str, t_protocol: IPProtocol, payload: &[u8], source_addr: IpAddr, dest_addr: IpAddr) -> Result<Packet, ParserError> {
+    match t_protocol {
+        IPProtocol::TCP => {
+            tcp_packet(interface_name, source_addr, dest_addr, payload)
+        },
+        IPProtocol::UDP => {
+            udp_packet(interface_name, source_addr, dest_addr, payload)
+        },
+        IPProtocol::ICMP => {
+            icmp_packet(interface_name, source_addr, dest_addr, payload)
+        },
+        IPProtocol::Other(..) => {
+            generic_t_packet(interface_name, source_addr, dest_addr, payload)
+        }
+        _ => Err(
+            ParserError::TransportProtocolError
+        )
+    }
+}
+
+// Function to parse well-known application protocols
+fn application_protocol_parser(port: &u16) -> String {
+    match port {
+        7 => "ECHO",
+        20 => "FTP Data",
+        21 => "FTP Control",
+        22 => "SSH",
+        23 => "Telnet",
+        25 => "SMTP",
+        53 => "DNS",
+        67 => "BOOTP/DHCP Server",
+        68 => "BOOTP/DHCP Client",
+        69 => "TFTP",
+        80 => "HTTP",
+        88 => "Kerberos",
+        110 => "POP",
+        123 => "NTP",
+        143 => "IMAP4",
+        443 => "HTTPS",
+        465 => "SMTP",
+        853 => "DNSoverTLS",
+        993 => "IMAP4overTLS",
+        995 => "POP3overTLS",
+        _ => "unknown"
+    }.to_string()
+}
+
+fn icmp_code_parser<'a>(code: IcmpCode) -> &'a str {
+    match code {
+        IcmpCode::EchoReply => "Echo Reply",
+        IcmpCode::Reserved => "Reserved",
+        IcmpCode::DestinationUnreachable(_some) => "Destination Unreachable",
+        IcmpCode::SourceQuench => "Source Quench",
+        IcmpCode::Redirect(_some) => "Redirect",
+        IcmpCode::EchoRequest => "Echo Request",
+        IcmpCode::RouterAdvertisment => "Router Advertisement",
+        IcmpCode::RouterSolicication => "Router Solicitation",
+        IcmpCode::TimeExceeded(_some) => "Time Exceeded",
+        IcmpCode::ParameterProblem(_some) => "Parameter Problem",
+        IcmpCode::Timestamp => "Timestamp",
+        IcmpCode::TimestampReply => "Timestamp Reply",
+        IcmpCode::ExtendedEchoRequest => "Extended Echo Request",
+        IcmpCode::ExtendedEchoReply(_some) => "Extended Echo Reply",
+        IcmpCode::Other(_some) => "Unknown"
     }
 }
